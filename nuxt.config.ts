8ce999cb49cf4ac11dcc255916a39a9d78500c1f
Gerson Garayar <garayargerson20@gmail.com>
2023-03-29 15:04:12 -0500

    fix: corrigiendo pagina detalle del producto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  /* routeRules:{
+    "/productos/*":{ssr:false},
+    "/api/*":{cors: true}
+  } */
 });
--- pages/productos/[id].vue
+++ pages/productos/[id].vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { useCart } from "~~/stores/cart";
+//import { useCart } from "~~/stores/cart";
 
 const route = useRoute();
 // console.log(route.params.id);
-const producto = ref();
+/* const producto = ref(); */
 
-const { pending, error, refresh } = await useLazyAsyncData(
+const { data: producto, pending } = await useFetch(`/api/products/${route.params.id}`)
+//console.log(producto);
+/* await useAsyncData(
   "product",
   async () => {
     fetch(`/api/products/${route.params.id}`)
@@ -15,38 +17,37 @@ const { pending, error, refresh } = await useLazyAsyncData(
       });
   },
   { server: false }
-);
+); */
 
-const { addProducto, products } = useCart();
+//const { addProducto, products } = useCart();
 
 const addCesta = () => {
-  addProducto(producto.value);
+  console.log("hola mundo");
+  //addProducto(producto.value);
 };
 
 const { data } = await useFetch("/api/products");
 </script>
 <template>
-  <p v-if="pending">
-    <span>cargando</span>
-  </p>
-  <p v-else-if="error">Error while fetching feed 💔</p>
+  <div v-if="pending">cargando</div>
+
   <main v-else class="max-w-7xl m-auto px-4 md:px-8">
     <section class="flex flex-col md:flex-row gap-4">
       <section class="md:hidden block">
-        <h2 class="text-2xl">{{ producto.nombre }}</h2>
-        <p>{{ producto.categoria }}</p>
-        <p>{{ producto.precio }} €</p>
+        <h2 class="text-2xl">{{ producto?.nombre }}</h2>
+        <p>{{ producto?.categoria }}</p>
+        <p>{{ producto?.precio }} €</p>
       </section>
 
       <section class="w-full">
-        <img :src="producto.imagen[0]" />
+        <img :src="producto?.imagen[0]" />
       </section>
 
       <aside class="shrink-0 md:w-96 flex flex-col gap-4">
         <div class="hidden md:block">
-          <h2 class="text-2xl">{{ producto.nombre }}</h2>
-          <p>{{ producto.nombre }}</p>
-          <p>{{ producto.precio }} €</p>
+          <h2 class="text-2xl">{{ producto?.nombre }}</h2>
+          <p>{{ producto?.nombre }}</p>
+          <p>{{ producto?.precio }} €</p>
         </div>
         <div class="flex flex-col gap-3">
           <button
@@ -63,7 +64,7 @@ const { data } = await useFetch("/api/products");
         </div>
         <div>
           <p>
-            {{ producto.descripcion }}
+            {{ producto?.descripcion }}
           </p>
         </div>
         <Filtro title="Evaluaciones">
